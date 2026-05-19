06a9dd9df5dca9fa328d3e3dae636da68765874e
Газимов Диас <gvzumdias@gmail.com>
2026-05-19 14:16:15 +0500

    add basic CLI structure

---
+++ task-cli.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	lenofargs := len(os.Args)
+	if lenofargs < 2 {
+		log.Fatal("Usage: task-cli <command>")
+	} else {
+		command := os.Args[1]
+		switch command {
+		case "add":
+			fmt.Println("add")
+		case "delete":
+			fmt.Println("delete")
+		case "update":
+			fmt.Println("update")
+		case "list":
+			fmt.Println("list")
+		case "mark-done":
+			fmt.Println("mark-done")
+		case "mark-in-progress":
+			fmt.Println("mark-in-progress")
+		default:
+			fmt.Println("unknown command")
+		}
+	}
+
+}
